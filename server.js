ff665b2c6c2ae101d5deb62f813a3cd21e045879
LostIrishman <jim@morancloud.com>
2015-03-17 16:05:32 -0500

    Implemented initial server side get and post code

--- server.js
+++ server.js
@@ -14,13 +14,29 @@ server.listen(3000);*/
 var http = require("http");
 var express = require('express');
 var app = express();
+var bodyParser = require('body-parser');
+/*var multer = require('multer');*/
 
+
+//Opt into services
+/*app.use(express.urlencoded());
+app.use(express.cookieParser());*/
+
+
+app.use(bodyParser.json()); // for parsing application/json
+/*app.use(bodyParser.urlencoded({ extended: true })); // for parsing application/x-www-form-urlencoded
+app.use(express.session({secret: "test"}));
+app.use(flash());*/
+
+/*app.use(multer());*/   // for parsing multipart/form-data
+
+
+//Set the public static resource folder
 app.use(express.static(__dirname));
-var server = http.createServer(app);
-server.listen(3000);
 
+
+// Manage routes
 app.get ("/", function(req,res){
-
    res.render("home", {title:"", message:"Hi"});
 });
 
@@ -73,6 +89,61 @@ app.get ("/api/devices", function(req,res){
 });
 
 
+app.get ("/api/users", function(req,res){
+    res.set("Content-Type", "application/json");
+    res.send({title:"test", message:"Hi"});
+});
+
+
+app.post('/api/test', function(req, res) {
+/*    var username = req.body.username;*/
+    var username = 'test';
+    console.log("post received: %s", username);
+    console.log(req.body);
+});
+
+
+app.post("/api/devices:deviceId", function (req, res){
+
+    var deviceName = req.params.deviceName;
+
+/*    var deviceToInsert = {
+        "deviceId":5,
+        "deviceName": "Device5",
+        "deviceHouseCodeId":"B",
+        "deviceCodeId": "2",
+        "deviceLocation":"Kitchen",
+        "deviceState": "OFF",
+        "deviceImagePath":"app/images/house.jpg"
+    };*/
+
+    var deviceToInsert = {
+        "deviceId":req.body.deviceId,
+        "deviceName": req.body.deviceName,
+        "deviceHouseCodeId":req.body.deviceHouseCodeId,
+        "deviceCodeId": req.body.deviceCodeId,
+        "deviceLocation": req.body.deviceLocation,
+        "deviceState": req.body.deviceState,
+        "deviceImagePath": req.body.deviceImagePath
+    };
+
+/*    data.addDevice(deviceName, deviceToInsert, function (err){
+        if(err) {
+            res.send(400, "Failed to Add device to db");
+        } else {*/
+
+    //Tue, 17 Mar 2015 20:08:01 GMT express deprecated res.send(status, body): Use res.status(status).send(body) instead at server.js:128:17
+            res.set("Content-Type", "application/json");
+            res.send(201, deviceToInsert);
+/*        }
+    });*/
+});
+
+
+/*app.engine(".html", require)*/
+
+
+
 
 /*app.post ("/api/devices:deviceId", function(req, res){
     var device = angular.fromJson(req);
@@ -101,3 +172,7 @@ app.get ("/api/devices", function(req,res){
 mongoose.connect(config.db, config.MongoOptions);
 */
 
+
+
+var server = http.createServer(app);
+server.listen(3000);
